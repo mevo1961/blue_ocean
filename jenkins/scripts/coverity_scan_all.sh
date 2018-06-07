49f96b74e1a4767614d32d3c233c9148e201af11
Meinolf Vogt <meinolf.vogt@nokia.com>
2018-06-07 13:22:38 +0200

    added source project; simplified coverity_scan_all.sh

--- jenkins/scripts/coverity_scan_all.sh
+++ jenkins/scripts/coverity_scan_all.sh
@@ -28,26 +28,20 @@ set -aeu
 
 ## @fn      do_setup()
 #  @brief   set the required variables to run a coverity test
-#  @param   {target_type}  target_type to build, e.g. FSM-r3
-#  @param   {module_list} the list of modules to be tested with Coverity, e.g. "ddal kernelmodules"
 #  @param   {server} set to 'y' if results should be commited to server
 #  @return  <none>
 do_setup()
 {
-    local target_type="${1:-FSM-r4}"
     # COVERITY_INSTALLATION_PATH will be set in Jenkins global variables
     COV_INSTALL_DIR=${COVERITY_INSTALLATION_PATH:-"/opt/coverity/x86_64/8.7.1"}
     COV_ROOTDIR=$(pwd)
-    COV_WORKDIR="$COV_ROOTDIR/COV_WORKDIR"
+    COV_WORKDIR="$COV_ROOTDIR/../data/COV_WORKDIR"
     COV_CONFIG_DIR="$COV_WORKDIR"
-    COV_RESULTS_DIR="$COV_WORKDIR/coverity-results/$target_type"
-    COV_REPORT_DIR="$COV_WORKDIR/coverity-report/$target_type/html"
+    COV_RESULTS_DIR="$COV_WORKDIR/coverity-results"
+    COV_REPORT_DIR="$COV_WORKDIR/coverity-report/html"
     COV_CONFIG="$COV_CONFIG_DIR/coverity-config.xml"
-    COV_PREVIEW_PARSER="./tools/cov_commit_preview_parser.py"
+    COV_PREVIEW_PARSER="./cov_commit_preview_parser.py"
     COV_ERRORS_PER_MODULE=""
-    COV_TARGET=""
-    BITBAKE_TARGET=""
-    COV_MODULE="${2:-ddal}"
     COV_CONNECT_SERVER=escovsub1.emea.nsn-net.net
     COV_CONNECT_PORT=8080
     COV_CONNECT_HTTPS_PORT=8443
@@ -55,33 +49,9 @@ do_setup()
     COV_BUILD_TIMEOUT=1800
     COV_API_SERVER=135.3.26.236/~coverity/cgi-bin/cov_cids_report.pl
     COV_KEYFILE="$HOME/.coverity/authkeys/ak-escovsub1.emea.nsn-net.net-8080"
-    USE_COVERITY_CONNECT="${3:-n}"
-    BRANCH_NAME="${4:-master}"
+    USE_COVERITY_CONNECT="${1:-n}"
 
-    case $target_type in
-        FSM-r3)
-            COV_TARGET="mips64-octeon2-linux-gnu"
-            BITBAKE_TARGET="fsmr3"
-            ;;
-        FSM-r4)
-            COV_TARGET="arm-cortexa15-linux-gnueabihf"
-            BITBAKE_TARGET="fsmr4-axm"
-            ;;
-        Lionfish)
-            COV_TARGET="aarch64-linux-gnu"
-            BITBAKE_TARGET="fsm-lionfish"
-            ;;
-        AirScale5G)
-            COV_TARGET="x86_64-pc-linux-gnu" 
-            BITBAKE_TARGET="airscale5g_xeon"
-            ;;
-        *)
-            COV_TARGET="unknown_COV_TARGET"
-            BITBAKE_TARGET="unknown_BITBAKE_TARGET"
-            ;;
-    esac
-
-    COV_STREAM=${BRANCH_NAME}--${COV_MODULE}--${COV_TARGET}
+    COV_STREAM=mevo-test-blue_ocean
 
     export PATH="$COV_INSTALL_DIR/bin:$PATH"
 
@@ -89,7 +59,7 @@ do_setup()
     rm -rf  $COV_REPORT_DIR
     mkdir -p $COV_WORKDIR $COV_CONFIG_DIR $COV_RESULTS_DIR $COV_REPORT_DIR
 
-    cov-configure --config $COV_CONFIG --compiler $COV_TARGET-gcc --comptype gcc --template
+    cov-configure --config $COV_CONFIG --compiler gcc --comptype gcc --template
 }
 
 ## @fn      build_cmd()
@@ -98,85 +68,28 @@ do_setup()
 #  @return  <none>
 build_cmd()
 {
-    local module=$1
-    case $module in
-        ddal)           BITBAKE_BUILD="y" ;;
-        kernel-modules) BITBAKE_BUILD="y" ;;
-        *)              BITBAKE_BUILD="n" ;;
-    esac
-    if [[ ${BITBAKE_BUILD} == "y" ]]; then
-        echo "#!/bin/bash"  > ./build_${module}.sh
-        echo "date"
-        chmod 777 ./build_${module}.sh
-        echo "./build_${module}.sh"
-    else
-        echo "./build ${module} $COV_TARGET --dirty --force $@"
-    fi
+    echo "#!/bin/bash"  > ./build.sh
+    echo "make -C ../src clean" >> ./build.sh
+    echo "make -C ../src" >> ./build.sh
+    chmod 777 ./build.sh
+    echo "./build.sh"
 }
 
 ## @fn      run_coverity()
 #  @brief   run a coverity test
-#  @param   {module} the module to be tested with Coverity, e.g. "ddal"
 #  @return  <none>
 run_coverity()
 {
-    local module="$1"
-    local results_dir="$COV_RESULTS_DIR/$module"
-    local report_dir="$COV_REPORT_DIR/$module"
-    local cov_strippath
-    case $module in
-        ddal)          BITBAKE_BUILD="y" ;;
-        kernelmodules) BITBAKE_BUILD="y" ;;
-        *)             BITBAKE_BUILD="n" ;;
-    esac
+    local results_dir="$COV_RESULTS_DIR/src"
+    local report_dir="$COV_REPORT_DIR/src"
 
     mkdir -p $results_dir $report_dir
 
-    # make a first build to make sure that only the required module will be built during cov-build command below
-    if [[ ${BITBAKE_BUILD} == "y" ]] ; then
-        if [[ "${module}" == "kernelmodules" ]] ; then
-            bbmodule='kernel-modules'
-        else
-            bbmodule="${module}"
-        fi
-
-        echo "building dependencies"
-        bash -c "date"
-    else
-        timeout -s 9 ${COV_BUILD_TIMEOUT} $(build_cmd $module)
-    fi
-
     # now build and analyze module with Coverity
-    if [[ ${BITBAKE_BUILD} == "y" ]]; then
-        if [[ "${module}" == "kernelmodules" ]] ; then
-            bbmodule='kernel-modules'
-        else
-            bbmodule="${module}"
-        fi
-
-        if [[ "${USE_COVERITY_CONNECT}" == "y" ]] ; then
-            # only do this if called from Jenkins:
-            # make a dummy commit to force building during the following cov_build command
-            echo executing dummy commit to force bitbake to build ${module}
-            cd src/${module}
-            git commit --allow-empty -m 'dummy commit to force building of submodule'
-            cd -
-        fi
-
-        echo now building with command: cov-build --config $COV_CONFIG --dir "$results_dir" $(build_cmd ${bbmodule})
-        # should be executed with a timeout, but somehow this does not work with bitbake
-        cov-build --config $COV_CONFIG --dir "$results_dir" $(build_cmd ${bbmodule})
-    else
-        # make sure that module is really built (i.e. make it "dirty")
-        touch "${COV_ROOTDIR}/src/$module/.dirty"
-        echo "now building with command $(build_cmd $module)"
-
-        timeout -s 9 ${COV_BUILD_TIMEOUT} cov-build --config $COV_CONFIG --dir "$results_dir" $(build_cmd $module --nodeps)
-        # make module source dir clean again
-        rm "${COV_ROOTDIR}/src/$module/.dirty"
-    fi
-    cov_strippath="$COV_ROOTDIR/builds/$COV_TARGET/$module/"
-    cov-analyze --all --dir "$results_dir"  --strip-path="$cov_strippath"
+    echo now building with command: cov-build --config $COV_CONFIG --dir "$results_dir" $(build_cmd)
+    # should be executed with a timeout, but somehow this does not work with bitbake
+    cov-build --config $COV_CONFIG --dir "$results_dir" $(build_cmd)
+    cov-analyze --all --dir "$results_dir"
 
 
     # commit the results (if required) and represent them as html
@@ -190,10 +103,10 @@ run_coverity()
             timeout -s 9 ${COV_CONNECT_SERVER_TIMEOUT} cov-commit-defects --ssl --on-new-cert trust --auth-key-file "$COV_KEYFILE" --dir "$results_dir" --host "$COV_CONNECT_SERVER" --https-port ${COV_CONNECT_HTTPS_PORT} --stream "$COV_STREAM" --scm git
         fi
         curl -s -S -o "$report_dir/index.html" "http://$COV_API_SERVER?req=show_cids&parms=cov_parms_espoo&mode=stream&name=${COV_STREAM}&comp=all&status=both"
-        COV_ERRORS_PER_MODULE="$COV_ERRORS_PER_MODULE $module:"$(cov_connect_errors_num "$report_dir")
+        COV_ERRORS_PER_MODULE="$COV_ERRORS_PER_MODULE src:"$(cov_connect_errors_num "$report_dir")
     else
         cov-format-errors --dir "$results_dir" --html-output "$report_dir" --strip-path="$cov_strippath"
-        COV_ERRORS_PER_MODULE="$COV_ERRORS_PER_MODULE $module:"$(cov_errors_num "$report_dir")
+        COV_ERRORS_PER_MODULE="$COV_ERRORS_PER_MODULE src:"$(cov_errors_num "$report_dir")
         cp -f "$COV_INSTALL_DIR/dtd/config.dtd" "$report_dir"
     fi
 }
@@ -299,7 +212,7 @@ check_committing_necessary()
     echo found ${ISSUES_IN_DATABASE} issues in database
 
     # create a preview of defects in current workspace
-    cov-commit-defects --ssl --on-new-cert trust --auth-key-file "$COV_KEYFILE" --dir "$results_dir" --host ${COV_CONNECT_SERVER} --https-port ${COV_CONNECT_HTTPS_PORT} --stream $COV_STREAM --preview-report-v2 $preview_file
+    cov-commit-defects --ssl --on-new-cert trust --auth-key-file "$COV_KEYFILE" --dir "$COV_RESULTS_DIR" --host ${COV_CONNECT_SERVER} --https-port ${COV_CONNECT_HTTPS_PORT} --stream $COV_STREAM --preview-report-v2 $preview_file
 
 
     ISSUES_IN_CURRENT_ANALYSIS=$(grep -c '"cid" :' ${preview_file})
@@ -324,9 +237,9 @@ check_committing_necessary()
 #----------------------------------------------------------------------------
 # execution starts here
 #----------------------------------------------------------------------------
-do_setup "${1:-FSM-r4}" "${2:-ddal}"  "${3:-n}" "${4:-master}"
+do_setup  "${1:-n}" 
 
-run_coverity $COV_MODULE
+run_coverity
 
 create_index_html $COV_ERRORS_PER_MODULE
 
